014209ef36a68347919306c480f216ef3865ddf9
Sparsh23AK <sparshgiri23@gmail.com>
2023-01-07 11:55:11 +0530

    Initializing Backend V2

---
+++ backend/src/test/java/com/sghc/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sghc.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
